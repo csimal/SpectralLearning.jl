0b3f533b76eb78e7f456f7b296f409ae9da170ed
Documenter.jl <documenter@juliadocs.github.io>
2022-03-17 10:43:18 +0000

    build based on ee935fa

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SpectralLearning","category":"page"},{"location":"#SpectralLearning","page":"Home","title":"SpectralLearning","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SpectralLearning.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SpectralLearning]","category":"page"}]
+}
